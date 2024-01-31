ac37be2835afce7ebeaaf04ebd656179312d2984
solarnius <solarnius@sagods.com>
2024-01-31 15:57:46 -0800

    new site who dis

--- solarni.us/next.config.mjs
+++ solarni.us/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "bafybeih3w6jc3tnw4lrok5ewtgcnoxer76ef5tdxkj773fyhzvyk2x3tgq.ipfs.nftstorage.link",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
