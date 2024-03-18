cea4885ba29e46f52e3d75532afa1c36c2834175
solarnius <solarnius@sagods.com>
2024-03-17 17:58:58 -0700

    annoying

--- solarni.us/next.config.mjs
+++ solarni.us/next.config.mjs
@@ -4,8 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname:
-          "bafybeih3w6jc3tnw4lrok5ewtgcnoxer76ef5tdxkj773fyhzvyk2x3tgq.ipfs.nftstorage.link",
+        hostname: "shdw-drive.genesysgo.net",
         port: "",
       },
     ],
